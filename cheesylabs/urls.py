91dce5a184cc4f9cbd81b022b14d6a2d26632846
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-11-10 13:43:36 +0100

    added published site

--- cheesylabs/urls.py
+++ cheesylabs/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django_distill import distill_path
 from . import views
 
+def get_index_params():
+    return None
+
 urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
+    distill_path('', views.landing_page, name='landing_page', distill_func=get_index_params),
     path('api/', views.api_view, name='api_view'),  # Keep your API view if needed
 ]
